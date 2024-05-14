f5cfc92de30084e14844c7a78282ce2c5f5bd6fb
Tristan Fox <foxtristan@proton.me>
2024-05-14 11:19:13 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,36 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      flake = false;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils = {
+      url = "github:numtide/flake-utils";
     };
   };
 
   outputs =
-    inputs@{
-      self,
+    {
       nixpkgs,
-      flake-utils,
+      utils,
+      rust-overlay,
       ...
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+    utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        gitignoreSrc = pkgs.callPackage inputs.gitignore { };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
-      rec {
-        packages.hello = pkgs.callPackage ./default.nix { inherit gitignoreSrc; };
-
-        legacyPackages = packages;
-
-        defaultPackage = packages.hello;
-
+      {
         devShell = pkgs.mkShell {
-          CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-
           buildInputs = with pkgs; [
-            cargo
-            rustc
-            git
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" ];
+              targets = [ ];
+            })
+            nil
           ];
         };
         formatter = pkgs.nixfmt-rfc-style;
